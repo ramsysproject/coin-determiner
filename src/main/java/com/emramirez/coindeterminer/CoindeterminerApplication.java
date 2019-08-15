0979e72554f60cfe7ed6564abbe011ef64fad350
ramsysproject <m.ramirez05@gmail.com>
2019-08-15 17:36:13 -0300

    Initial project setup

---
+++ src/main/java/com/emramirez/coindeterminer/CoindeterminerApplication.java
@@ -0,0 +1,13 @@
+package com.emramirez.coindeterminer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoindeterminerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoindeterminerApplication.class, args);
+	}
+
+}
